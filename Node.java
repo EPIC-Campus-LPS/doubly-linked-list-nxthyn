87b6705d53b88152a2d06617efce264c9b745ca4
nathanhoehndorf <hoehndorf.nathan@gmail.com>
2023-10-13 11:26:39 -0600

    started outlining what methods will be needed for Node class; created blank DoublyLinkedList class

---
+++ Node.java
@@ -0,0 +1,53 @@
+public class Node<E> {
+
+    private E node;
+
+
+    public Node() {
+
+        node = null;
+
+    }
+    public Node(E n) {
+
+        node = n;
+
+    }
+
+    public E get() {
+
+        return node;
+
+    }
+
+    public void set(E n) {
+
+        node = n;
+
+    }
+
+    public E getPreviousNode() {
+
+
+
+    }
+
+    public E getNextNode() {
+
+
+
+    }
+
+    public void setPreviousNode(E n) {
+
+
+
+    }
+
+    public void setNextNode(E n) {
+
+
+
+    }
+
+}
